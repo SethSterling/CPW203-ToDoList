09bdae322da9f3699c2261935971afd13d650731
SethSterling <sethbdsterling@gmail.com>
2020-05-12 16:36:07 -0700

    Added ToDoList code.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,65 @@
 var picker = datepicker("#due-date");
 picker.setMin(new Date());
+window.onload;
+{
+    var addItemBtn = getById("add-item");
+    addItemBtn.onclick = getToDoItem;
+}
+var ToDoItem = (function () {
+    function ToDoItem(title, dueDate, isCompleted) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.isCompleted = isCompleted;
+    }
+    return ToDoItem;
+}());
+function getById(id) {
+    return document.getElementById(id);
+}
+function isValid(title, dueDate) {
+    var isValid = true;
+    if (title.trim() == "") {
+        displayError("title", "Title is Required");
+        isValid = false;
+    }
+    if (dueDate.trim() == "") {
+        displayError("due-date", "A due date is Required");
+        isValid = false;
+    }
+    return isValid;
+}
+function displayError(errorType, errorMsg) {
+    var error = getById(errorType).nextElementSibling;
+    error.innerHTML = errorMsg;
+}
+function clearErrors() {
+    displayError("title", "*");
+    displayError("due-date", "*");
+}
+function getToDoItem() {
+    clearErrors();
+    var itemTitle = getById("title").value;
+    var itemDueDate = getById("due-date").value;
+    var itemCompleted = getById("is-complete").checked;
+    if (isValid(itemTitle, itemDueDate)) {
+        var item = new ToDoItem(itemTitle, itemDueDate, itemCompleted);
+        displayToDoItem(item);
+    }
+}
+function displayToDoItem(item) {
+    var toDoList = getById("ToDo");
+    var toDoItem = document.createElement("div");
+    toDoList.appendChild(toDoItem);
+    var toDoTitle = document.createElement("h1");
+    toDoTitle.innerText = item.title;
+    toDoItem.appendChild(toDoTitle);
+    var toDoDueDate = document.createElement("p");
+    toDoDueDate.innerText = "Due: " + item.dueDate;
+    toDoItem.appendChild(toDoDueDate);
+    if (item.isCompleted) {
+        toDoItem.setAttribute("id", "isCompleted");
+    }
+    else {
+        toDoItem.setAttribute("id", "notCompleted");
+    }
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,98 @@
 // @ts-ignore: Ignoring issue with js-datepiker lack of intellisense
 const picker = datepicker("#due-date");
 picker.setMin(new Date()); // Set to today's date
+
+window.onload{
+    let addItemBtn = getById("add-item");
+    addItemBtn.onclick = getToDoItem;
+}
+
+class ToDoItem {
+    title:string;
+    dueDate:String;
+    isCompleted:boolean;
+
+    constructor(title:string, dueDate:String, isCompleted:boolean){
+        this.title = title;
+        this.dueDate = dueDate;
+        this.isCompleted = isCompleted;
+    }
+}
+function getById(id:string) {
+    return document.getElementById(id);
+}
+
+/**
+ * Checks if the data is valid
+ */
+function isValid(title:string, dueDate:String):boolean {
+    let isValid = true;
+    if(title.trim() == ""){
+        displayError("title", "Title is Required");
+        isValid = false;
+    }
+
+    if(dueDate.trim() == ""){
+        displayError("due-date", "A due date is Required");
+        isValid = false;
+    }
+    
+    return isValid;
+}
+
+/**
+ * Displays an error message.
+ * @param errorType The id of the HTML element
+ * @param errorMsg The messeage that will be displayed
+ */
+function displayError(errorType:string, errorMsg:string) {
+    let error = (<HTMLElement>getById(errorType)).nextElementSibling;
+    error.innerHTML = errorMsg;
+}
+
+function clearErrors() {
+    displayError("title", "*");
+    displayError("due-date", "*");
+}
+
+/**
+ * Creates ToDoItem object
+ */
+function getToDoItem() {
+    clearErrors();
+    let itemTitle = (<HTMLInputElement>getById("title")).value;
+    let itemDueDate = (<HTMLInputElement>getById("due-date")).value
+    let itemCompleted = (<HTMLInputElement>getById("is-complete")).checked
+
+    if(isValid(itemTitle, itemDueDate)){
+        let item = new ToDoItem(itemTitle, itemDueDate, itemCompleted);
+        displayToDoItem(item);
+    }   
+}
+
+/**
+ * Displays the item that was created.
+ * @param item 
+ */
+function displayToDoItem(item:ToDoItem):void {
+    let toDoList = getById("ToDo");
+    let toDoItem = document.createElement("div");
+    toDoList.appendChild(toDoItem)
+
+    let toDoTitle = document.createElement("h1");
+    toDoTitle.innerText = item.title;
+    toDoItem.appendChild(toDoTitle);
+
+    let toDoDueDate = document.createElement("p");
+    toDoDueDate.innerText = "Due: " + item.dueDate;
+    toDoItem.appendChild(toDoDueDate);
+
+    if(item.isCompleted){
+        toDoItem.setAttribute("id", "isCompleted");
+    }
+    else{
+        toDoItem.setAttribute("id", "notCompleted");
+    }
+}
+
+// Task: Allow user to mark a ToDoItem as Completed
